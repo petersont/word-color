8b7220b80689b78b147f1b41034efa64fac71660
The Dragon <dragon@2-complex.com>
2016-10-12 23:20:13 +0000

    Adding wordcolor.py

--- wordcolor.py
+++ wordcolor.py
@@ -5,6 +5,9 @@ import hashlib
 def hash_string(s):
     return int(int(hashlib.md5(s).hexdigest(), 16)%360)
 
+def get_scopename(h):
+    return "explicit-hue-"+str(h)
+
 class ColorerListener(sublime_plugin.EventListener):
     def redo(self, view):
         word_regions = view.find_all("[_A-Za-z][_a-zA-Z0-9]*")
@@ -14,9 +17,11 @@ class ColorerListener(sublime_plugin.EventListener):
             h = hash_string(view.substr(region).encode('utf-8'))
             hash_to_region_list[h] = hash_to_region_list.get(h, []) + [region]
 
+        for h in range(0, 360):
+            view.erase_regions(get_scopename(h))
+
         for h, regions in hash_to_region_list.items():
-            scopename = "explicit-hue-"+str(h)
-            view.erase_regions(scopename)
+            scopename = get_scopename(h)
             view.add_regions(scopename, regions, scopename, "", sublime.DRAW_NO_OUTLINE)
 
     def on_modified(self, view):
@@ -24,3 +29,4 @@ class ColorerListener(sublime_plugin.EventListener):
 
     def on_activated(self, view):
         self.redo(view)
+
